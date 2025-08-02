2e651d9397921be5a9c685a1f1fd0294052f1e7a
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-08-02 12:52:27 +0300

    Added Deployment Environment Configurations

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,36 @@
+export const environment = {
+  production: false,
+  version: '2.0.0',
+  copyright: '2021-25',
+  github: 'https://github.com/benedictgodfreym',
+  api: {
+    tmdb: {
+      url: `https://api.themoviedb.org/3`,
+      img: {
+        url: `https://image.tmdb.org/t/p`,
+        sizes: { original: `original` },
+      },
+      key: '52bb64cfc4910dbb62742f2e0342bb58',
+      site: 'https://www.themoviedb.org'
+    },
+    torrent: {
+      url: `https://torrent-api-py-nx0x.onrender.com/api/v1`,
+      github: 'https://github.com/Ryuk-me/Torrent-Api-py'
+    },
+    omdb: {
+      url: `https://www.omdbapi.com`,
+      key: `b9dccd20`,
+      site: 'https://www.omdbapi.com'
+    },
+    country_codes: {
+      url: {
+        iso3: `http://country.io/iso3.json`,
+        names: `http://country.io/names.json`
+      }
+    }
+  },
+  resources: {
+    loadingImage: `img/default-image.jpg`,
+    errorImage: `img/no-image.jfif`
+  }
+};
