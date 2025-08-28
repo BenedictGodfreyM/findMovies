c4e9c9d6646bbd3a4dccd017094ec5e987232b3e
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-08-28 16:26:22 +0300

    Registered routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
+import { AboutUsComponent, HomeComponent, MovieDetailsComponent, MoviesComponent, PageNotFoundComponent, SearchComponent, TvshowDetailsComponent, TvshowsComponent } from './pages';
+import { TMDBMovieResolver, TMDBSeriesResolver } from './resolvers';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+    title: 'About Us'
+  },
+  {
+    path: 'movies/:tmdb_id',
+    component: MovieDetailsComponent,
+    resolve: {movie: TMDBMovieResolver},
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent,
+    title: 'Movies'
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
+    title: 'Search'
+  },
+  {
+    path: 'tv-shows/:tmdb_id',
+    component: TvshowDetailsComponent,
+    resolve: {tv_show: TMDBSeriesResolver},
+  },
+  {
+    path: 'tv-shows',
+    component: TvshowsComponent,
+    title: 'TV Shows'
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+    title: 'Page Not Found'
+  },
+];
