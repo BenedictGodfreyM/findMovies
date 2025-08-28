79c0512dc3b6f1e0b7d1fe03aa82f04c5e1f06d1
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-08-28 16:23:02 +0300

    Added Router & HTTP Client providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, TitleStrategy } from '@angular/router';
 
 import { routes } from './app.routes';
+import { PageTitleService } from './services';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    {provide: TitleStrategy, useClass: PageTitleService}
   ]
 };
