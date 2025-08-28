49fc77ebb94d89877f72936702d97d95a6eb1c4f
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-08-28 16:39:56 +0300

    Added pipes

---
+++ src/app/pipes/index.ts
@@ -0,0 +1,5 @@
+export * from './duration.pipe';
+export * from './format-countries.pipe';
+export * from './format-date.pipe';
+export * from './format-languages.pipe';
+export * from './safe.pipe';
