4b4bc846cccfbc02d99a304b38ad47e4e493c618
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-08-29 10:40:36 +0300

    Registered http interceptors & page animatiomations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,19 @@ import { provideRouter, TitleStrategy } from '@angular/router';
 
 import { routes } from './app.routes';
 import { PageTitleService } from './services';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { HeadersInterceptor, HttpCancelInterceptor } from './interceptors';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimations(),
+    {provide: HTTP_INTERCEPTORS, useClass: HeadersInterceptor},
+    {provide: HTTP_INTERCEPTORS, useClass: HttpCancelInterceptor},
     {provide: TitleStrategy, useClass: PageTitleService}
   ]
 };
