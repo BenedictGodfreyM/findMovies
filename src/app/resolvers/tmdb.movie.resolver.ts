2ddd1dc0160d022eabbbb8a93f8cbca76d859603
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-08-29 11:46:26 +0300

    Refactored resolvers

--- src/app/resolvers/tmdb.movie.resolver.ts
+++ src/app/resolvers/tmdb.movie.resolver.ts
@@ -1,16 +1,18 @@
 import { RedirectCommand, ResolveFn, Router } from "@angular/router";
 import { TMDBMovieDetails } from "../models";
 import { inject } from "@angular/core";
-import { NavigationService, TmdbService } from "../services";
-import { catchError, of } from "rxjs";
+import { NavigationService, TmdbService, UiLoaderService } from "../services";
+import { catchError, finalize, of } from "rxjs";
 
 export const TMDBMovieResolver: ResolveFn<TMDBMovieDetails|RedirectCommand> = (route) => {
     const TMDBMovieStore = inject(TmdbService);
     const NavigationStore = inject(NavigationService);
+    const UILoader = inject(UiLoaderService);
     const router = inject(Router);
     const tmdb_id = parseInt(route.paramMap.get('tmdb_id') || '');
 
-    return TMDBMovieStore.movie_details(tmdb_id).pipe(catchError(error => {
+    UILoader.showSpinner();
+    return TMDBMovieStore.movie_details(tmdb_id).pipe(finalize(() => UILoader.stopSpinner()),catchError(error => {
         console.error('Failed to load movie details:', error);
         return of(new RedirectCommand(router.parseUrl(NavigationStore.getReturnURL() || '/')))
     }));
--- src/app/resolvers/tmdb.series.resolver.ts
+++ src/app/resolvers/tmdb.series.resolver.ts
@@ -1,16 +1,18 @@
 import { RedirectCommand, ResolveFn, Router } from "@angular/router";
 import { TMDBSeriesDetails } from "../models";
 import { inject } from "@angular/core";
-import { NavigationService, TmdbService } from "../services";
+import { NavigationService, TmdbService, UiLoaderService } from "../services";
 import { catchError, of } from "rxjs";
 
 export const TMDBSeriesResolver: ResolveFn<TMDBSeriesDetails|RedirectCommand> = (route) => {
     const TMDBMovieStore = inject(TmdbService);
     const NavigationStore = inject(NavigationService);
+    const UILoader = inject(UiLoaderService);
     const router = inject(Router);
     const tmdb_id = parseInt(route.paramMap.get('tmdb_id') || '');
 
-    return TMDBMovieStore.tv_show_details(tmdb_id).pipe(catchError(error => {
+    UILoader.showSpinner();
+    return TMDBMovieStore.tv_show_details(tmdb_id).pipe(UILoader.stopSpinner(),catchError(error => {
         console.error('Failed to load tv show details:', error);
         return of(new RedirectCommand(router.parseUrl(NavigationStore.getReturnURL() || '/')))
     }));
