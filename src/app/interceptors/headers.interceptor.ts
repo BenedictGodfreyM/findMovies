4b6cbb31ae219d43a69deaf1bfc5fdaf2d153c09
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-10-02 20:37:55 +0300

    Refactored headers interceptor

--- src/app/interceptors/headers.interceptor.ts
+++ src/app/interceptors/headers.interceptor.ts
@@ -1,23 +1,10 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { HttpHeaders } from '@angular/common/http';
+import { HttpInterceptorFn } from '@angular/common/http';
 
-@Injectable()
-export class HeadersInterceptor implements HttpInterceptor {
-
-  constructor() {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    request = request.clone({
-      headers: new HttpHeaders({
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Accept, Content-Type, Origin',
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      })
-    });
-
-    return next.handle(request);
-  }
-}
+export const headersInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req.clone({setHeaders: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Accept, Content-Type, Origin',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }}));
+};
