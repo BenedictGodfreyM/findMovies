1aa21d99ece76018718db1ee70f4bf1209ee9cce
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-10-02 21:27:30 +0300

    Refactored tv-show details resolver

--- src/app/resolvers/tmdb.series.resolver.ts
+++ src/app/resolvers/tmdb.series.resolver.ts
@@ -2,7 +2,7 @@ import { RedirectCommand, ResolveFn, Router } from "@angular/router";
 import { TMDBSeriesDetails } from "../models";
 import { inject } from "@angular/core";
 import { NavigationService, TmdbService, UiLoaderService } from "../services";
-import { catchError, of } from "rxjs";
+import { catchError, finalize, of } from "rxjs";
 
 export const TMDBSeriesResolver: ResolveFn<TMDBSeriesDetails|RedirectCommand> = (route) => {
     const TMDBMovieStore = inject(TmdbService);
@@ -12,7 +12,7 @@ export const TMDBSeriesResolver: ResolveFn<TMDBSeriesDetails|RedirectCommand> = 
     const tmdb_id = parseInt(route.paramMap.get('tmdb_id') || '');
 
     UILoader.showSpinner();
-    return TMDBMovieStore.tv_show_details(tmdb_id).pipe(UILoader.stopSpinner(),catchError(error => {
+    return TMDBMovieStore.tv_show_details(tmdb_id).pipe(finalize(() => UILoader.stopSpinner()),catchError(error => {
         console.error('Failed to load tv show details:', error);
         return of(new RedirectCommand(router.parseUrl(NavigationStore.getReturnURL() || '/')))
     }));
